c288f61facb93b4434e0754f80f71df3550ee656
PattarapongB <92260311+PattarapongGitHub@users.noreply.github.com>
2024-09-16 09:37:33 +0700

    Add files via upload

---
+++ 23.SimpleCalculator.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        int firstone =  (first+second);
+        System.out.println(first + (" + ") + (second) + (" = ") + (firstone)) ;
+        
+        int secondone =  (first-second);
+        System.out.println(first + (" - ") + second + (" = ") + (secondone)) ;
+        
+        int thirdone =  (first*second);
+        System.out.println(first + (" * ") + second + (" = ") + (thirdone)) ;
+        
+        double forthone =  (double) 1.0 * first/second;
+        System.out.println(first + (" / ") + second + (" = ") + (forthone)) ;
+        
+    }
+}
---
+++ 5.Message.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        // Write your program here
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.println("Write a message:");
+        
+        String message = scanner.nextLine();
+        
+        System.out.println(message);
+        
+        
+        
+        
+        
+        
+
+      
+        
+
+    }
+}
